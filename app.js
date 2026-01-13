306b2cf78ccbb9bae5c1b22f27fdeb96857a5f8b
rebecca-aston <rebecca.l.aston@gmail.com>
2026-01-13 09:22:11 +0000

    use env variable for correct port on render

--- app.js
+++ app.js
@@ -6,13 +6,14 @@ const http = require("http");
 const server = http.createServer(app);//socket io needs an http server
 const { Server } = require("socket.io");
 const io = new Server(server);
+const port = process.env.PORT || 3000;
 
 // Tell our Node.js Server to host our P5.JS sketch from the public folder.
 app.use(express.static("public"));
 
 // Setup Our Node.js server to listen to connections from chrome, and open chrome when it is ready
-server.listen(3000, () => {
-  console.log("listening on *:3000");
+server.listen(port, () => {
+  console.log("listening on: "+port);
 });
 
 let printEveryMessage = false; 
